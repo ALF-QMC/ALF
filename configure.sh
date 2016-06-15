e93a8af1f60203fbfad6cbcf7041b8d667db786c
Johannes Hofmann (Uni-PC) <jhofmann@physik.uni-wuerzburg.de>
2016-06-15 15:25:00 +0200

    including some even more aggressive but still working intel specific compilation flags

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 export DIR=`pwd`
 export f90=mpiifort
 #export f90="gfortran"
-export FL="-g -c  -w  -O3 -xHost -unroll -finline-functions -heap-arrays 1024"
+export FL="-g -c  -w  -O3 -fp-model fast=2 -xHost -unroll -finline-functions -ipo -ip -heap-arrays 1024"
 export Libs=${DIR}"/Libraries/"
 export LIB_BLAS_LAPACK="-mkl=sequential"
 # uncomment next lines for hybrid of MPI and OpenMP
-export LIB_BLAS_LAPACK="-mkl=parallel -qopenmp"
-export FL="${FL} -qopenmp -parallel"
+#export LIB_BLAS_LAPACK="-mkl=parallel -qopenmp"
+#export FL="${FL} -qopenmp -parallel"
