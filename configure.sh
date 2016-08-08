f946d06a5b8faaabcd910b963bfacf8576a16aab
Johannes Hofmann (Jureca) <jhofmann@physik.uni-wuerzburg.de>
2016-08-08 11:28:02 +0200

    preparation of merge master (MaxEnt) -> SPT (Jureca)

--- configure.sh
+++ configure.sh
@@ -2,9 +2,9 @@ export DIR=`pwd`
 export f90=mpiifort
 #export f90="gfortran"
 
-export FL="-c  -O3  -fp-model fast=2 -xHost -unroll -finline-functions -ipo -ip -heap-arrays 1024"
+export FL="-c  -O3  -fp-model fast=2 -xHost -unroll -finline-functions -ipo -ip -heap-arrays 256"
 #uncomment the next line if you want to debug/profile your code
-export FL="${FL} -g"
+#export FL="${FL} -g  -traceback"
 
 export Libs=${DIR}"/Libraries/"
 export LIB_BLAS_LAPACK="-lblas -llapack"
