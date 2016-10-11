29bce1401c68ab6eefe2d90097a25b63dc217222
Johannes Hofmann (LRZ) <jhofmann@physik.uni-wuerzburg.de>
2016-10-11 21:23:29 +0200

    new configure files for automated compiling

--- configure.sh
+++ configure.sh
@@ -1,25 +1,5 @@
 export DIR=`pwd`
-export f90=mpiifort
-#export f90="gfortran"
-
-#LRZ enviroment
-export f90=mpif90
-module unload mpi.ibm
-module load mpi.intel
-module unload mkl
-module load mkl/2017_s
-
-export FL="-c  -O3  -fp-model fast=2 -xHost -unroll -finline-functions -ipo -ip -heap-arrays 1024"
-#uncomment the next line if you want to debug/profile your code
-export FL="${FL} -g -traceback"
-
+export f90="gfortran"
+export FL="-c  -O3"
 export Libs=${DIR}"/Libraries/"
-#export LIB_BLAS_LAPACK="-lblas -llapack"
-#export LIB_BLAS_LAPACK="-mkl=sequential"
-
-#uncomment the following line on LRZ
-export LIB_BLAS_LAPACK=$MKL_LIB
-
-# uncomment next lines for hybrid of MPI and OpenMP
-#export LIB_BLAS_LAPACK="-mkl=parallel -qopenmp"
-#export FL="${FL} -qopenmp -parallel"
+export LIB_BLAS_LAPACK="-lblas -llapack"
