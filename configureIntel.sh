6ad3e72b6bab42982e1476688da721947c940519
Johannes Hofmann (Laptop) <jhofmann@physik.uni-wuerzburg.de>
2016-10-14 00:58:24 +0200

    some change

--- configureIntel.sh
+++ configureIntel.sh
@@ -1,11 +1,11 @@
 export DIR=`pwd`
-export f90=mpiifort
+export f90=ifort
 #export f90="gfortran"
 
-export FL="-c -O3 -fp-model fast=2 -xHost -unroll -finline-functions -ipo -ip -heap-arrays 1024 -DMPI"
+export FL="-c -O3 -fp-model fast=2 -march=corei7-avx -unroll -finline-functions -ipo -ip -heap-arrays 1024 -DnMPI"
 
 #uncomment the next line if you want to debug/profile your code
-#export FL="${FL} -g -traceback"
+export FL="${FL} -pg"
 
 export Libs=${DIR}"/Libraries/"
 #export LIB_BLAS_LAPACK="-lblas -llapack"
