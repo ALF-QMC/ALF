725802684c5276cda995e2c4fa00a0374cef32ab
Johannes Hofmann (Uni-PC) <jhofmann@physik.uni-wuerzburg.de>
2016-10-26 20:14:53 +0200

    minor changes

--- configureIntel.sh
+++ configureIntel.sh
@@ -9,4 +9,5 @@ export FL="-c -O3 -fp-model fast=2 -xHost -unroll -finline-functions -ipo -ip -h
 
 export Libs=${DIR}"/Libraries/"
 #export LIB_BLAS_LAPACK="-lblas -llapack"
-export LIB_BLAS_LAPACK="-mkl=sequential"
+#export LIB_BLAS_LAPACK="-mkl=sequential"
+export LIB_BLAS_LAPACK="-mkl=parallel"
