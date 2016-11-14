70c3adec153b50b58130ec027630fdf08425434d
Johannes Hofmann (Uni-PC) <jhofmann@physik.uni-wuerzburg.de>
2016-11-14 11:59:13 +0100

    update configureIntel.sh

--- configureIntel.sh
+++ configureIntel.sh
@@ -2,12 +2,12 @@ export DIR=`pwd`
 export f90=mpiifort
 #export f90="gfortran"
 
-export FL="-c -O3 -fp-model fast=2 -xHost -unroll -finline-functions -ipo -ip -heap-arrays 1024 -DMPI"
+export FL="-c -O3 -fp-model fast=2 -xHost -unroll -finline-functions -ipo -ip -heap-arrays 1024 -no-wrap-margin -DMPI"
 
 #uncomment the next line if you want to debug/profile your code
-#export FL="${FL} -g -traceback"
+export FL="${FL} -g -traceback"
 
 export Libs=${DIR}"/Libraries/"
 #export LIB_BLAS_LAPACK="-lblas -llapack"
-#export LIB_BLAS_LAPACK="-mkl=sequential"
-export LIB_BLAS_LAPACK="-mkl=parallel"
+export LIB_BLAS_LAPACK="-mkl=sequential"
+#export LIB_BLAS_LAPACK="-mkl=parallel"
