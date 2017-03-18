507bed8f108ee8122cb47debc8d4ddde0a240250
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2017-03-18 18:59:49 +0100

    spelling

--- Prog/wrapur.f90
+++ Prog/wrapur.f90
@@ -1,5 +1,5 @@
 
-!  Copyright (C) 2016 2017 The ALF project
+!  Copyright (C) 2016, 2017 The ALF project
 ! 
 !     The ALF project is free software: you can redistribute it and/or modify
 !     it under the terms of the GNU General Public License as published by
