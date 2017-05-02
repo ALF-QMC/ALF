7fa50b2e52a699b4c1557b25137ef7f9bc3ee234
Florian Goth <CaptainSifff@gmx.de>
2017-05-02 14:44:32 +0200

    update test 4

--- testsuite/Prog.tests/4-opmultct.f90
+++ testsuite/Prog.tests/4-opmultct.f90
@@ -5,18 +5,21 @@
 Program OPMULTTEST
 !
       Use Operator_mod
+      Use MyMats
 !
 !
-      Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: U
+      Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: U, Uold, mytmp
       Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: V
       Complex (Kind=Kind(0.D0)), Dimension (5, 5) :: matnew, matold
-      Complex (Kind=Kind(0.D0)) :: tmp
+      Complex (Kind=Kind(0.D0)) :: tmp, Z
+      Real(Kind = Kind(0.D0)), Dimension(:), allocatable :: E
+      Complex(Kind=Kind(0.D0)), allocatable, dimension(:) :: work, TAU
       Integer, Dimension (:), Allocatable :: P
-      Integer :: i, j, n, opn, Ndim
-!
+      Integer :: i, j, n, opn, Ndim, info, lwork
 !
       Do opn = 1, 4
-         Allocate (U(opn, opn), V(opn, 5), P(opn))
+         lwork = 2* opn
+         Allocate (U(opn, opn), V(opn, 5), P(opn), work(lwork), Tau(opn), Uold(opn, opn), E(opn), myTMP(opn, opn))
          Ndim = 5
          Do i = 1, Ndim
             Do j = 1, Ndim
@@ -31,13 +34,34 @@ Program OPMULTTEST
             End Do
             P (i) = i
          End Do
-!
+
+         if(opn > 1) then
          Do i = 1, opn
             Do j = 1, opn
-               U (i, j) = CMPLX (i, j, kind(0.D0))
+               U (i, j) = CMPLX (i + j, j - i, kind(0.D0))
             End Do
          End Do
-!
+         CALL DIAG(U, mytmp, E)
+         U = mytmp
+         else
+         U(1, 1) = 1.D0
+         Uold(1, 1) = 1.D0
+         endif
+         if(opn > 1) then
+         Z = DET_C(mytmp, opn)
+         DO I = 1, opn
+            U(I, 1) = U(I, 1) / Z
+         ENDDO
+         Uold = U
+         TAU = 0.D0
+         if(opn > 2) then
+            CALL ZGEQRF(opn, opn, U, opn, TAU, WORK, LWORK, INFO)
+            DO I = 1, opn-1
+                U(I, opn) = TAU(I)
+            ENDDO
+            U(opn, opn) = 1.D0 - U(opn,opn)*(1.D0 - TAU(opn))! absorb last sign (stored in U(opn, opn)) into redefinition of tau
+         endif
+         endif
          Call opmultct (V, U, P, matnew, opn, Ndim)
 !
 ! check against old version
@@ -45,21 +69,25 @@ Program OPMULTTEST
             Do i = 1, Ndim
                tmp = CMPLX (0.d0, 0.d0, kind(0.D0))
                Do m = 1, opn
-                  tmp = tmp + conjg (U(n, m)) * V (m, i)
+                  tmp = tmp + conjg (Uold(n, m)) * V (m, i)
                End Do
                matold (i, P(n)) = tmp
             End Do
          End Do
 !
+write (*, *) "opn = ", opn
+write (*, *) (matold)
+write (*,*) "================================"
+write (*, *) (matnew)
          Do i = 1, Ndim
             Do j = 1, Ndim
-               If (matold(i, j) .Ne. matnew(i, j)) Then
+               If (Abs(matold(i, j) - matnew(i, j)) > MAX(ABS(matold(i, j)), ABS(matnew(i, j)))*1D-14) Then
                write (*,*) "ERROR"
                   Stop 2
                End If
             End Do
          End Do
-         Deallocate (U, V, P)
+         Deallocate (U, V, P, TAU, work, uold, E, mytmp)
       End Do
       write (*,*) "success"
 End Program OPMULTTEST
