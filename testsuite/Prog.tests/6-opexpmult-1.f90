cb94cf9652b157ffa01fe0b1d2fb93451d860113
Florian Goth <CaptainSifff@gmx.de>
2017-05-03 02:57:31 +0200

    fix remaining tests.

--- testsuite/Prog.tests/6-opexpmult-1.f90
+++ testsuite/Prog.tests/6-opexpmult-1.f90
@@ -10,17 +10,20 @@ Program OPEXPMULTTEST
       Use Operator_mod
 !
 !
-      Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: U
+      Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: U, Uold, mytmp
       Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: V
       Complex (Kind=Kind(0.D0)), Dimension (:), Allocatable :: Z
       Complex (Kind=Kind(0.D0)), Dimension (5, 5) :: matnew, matold
       Complex (Kind=Kind(0.D0)) :: tmp, lexp
+      Real(Kind = Kind(0.D0)), Dimension(:), allocatable :: E
+      Complex(Kind=Kind(0.D0)), allocatable, dimension(:) :: work, TAU
       Integer, Dimension (:), Allocatable :: P
-      Integer :: i, j, n, m, opn, Ndim
+      Integer :: i, j, n, m, opn, Ndim, info, lwork
 !
 !
       Do opn = 1, 4
-         Allocate (U(opn, opn), V(opn, 5), P(opn), Z(opn))
+         lwork = 2* opn
+         Allocate (U(opn, opn), V(opn, 5), P(opn), Z(opn), work(lwork), Tau(opn), Uold(opn, opn), E(opn), mytmp(opn, opn))
          Ndim = 5
          Do i = 1, Ndim
             Do j = 1, Ndim
@@ -36,13 +39,33 @@ Program OPEXPMULTTEST
             P (i) = i
             Z (i) = Exp (CMPLX(i, j, kind(0.D0)))
          End Do
-!
+        if(opn > 1) then
          Do i = 1, opn
             Do j = 1, opn
-               U (i, j) = CMPLX (i, j, kind(0.D0))
+               U (i, j) = CMPLX (i + j, j - i, kind(0.D0))
             End Do
          End Do
-!
+         CALL DIAG(U, mytmp, E)
+         U = mytmp
+         else
+         U(1, 1) = 1.D0
+         Uold(1, 1) = 1.D0
+         endif
+         if(opn > 1) then
+         lexp = DET_C(mytmp, opn)
+         DO I = 1, opn
+            U(I, 1) = U(I, 1) / lexp
+         ENDDO
+         Uold = U
+         TAU = 0.D0
+         if(opn > 2) then
+            CALL ZGEQRF(opn, opn, U, opn, TAU, WORK, LWORK, INFO)
+            DO I = 1, opn-1
+                U(I, opn) = TAU(I)
+            ENDDO
+            U(opn, opn) = 1.D0 - U(opn,opn)*(1.D0 - TAU(opn))! absorb last sign (stored in U(opn, opn)) into redefinition of tau
+         endif
+         endif
          Call opexpmult (V, U, P, matnew, Z, opn, Ndim)
 !
 ! check against old version
@@ -52,12 +75,16 @@ Program OPEXPMULTTEST
 !             Mat(I,Op%P(n))  =  ExpMOp(n) * zdotu(Op%N,Op%U(1,n),1,VH(1,I),1)
                tmp = CMPLX (0.d0, 0.d0, kind(0.D0))
                Do m = 1, opn
-                  tmp = tmp + V (m, i) * U (m, n)
+                  tmp = tmp + V (m, i) * Uold (m, n)
                End Do
                matold (i, P(n)) = lexp * tmp
             End Do
          End Do
 !
+write (*, *) "opn = ", opn
+write (*, *) (matold)
+write (*,*) "================================"
+write (*, *) (matnew)
          Do i = 1, Ndim
             Do j = 1, Ndim
                tmp = matold (i, j) - matnew (i, j)
@@ -72,7 +99,7 @@ Program OPEXPMULTTEST
 !
             End Do
          End Do
-         Deallocate (U, V, P, Z)
+         Deallocate (U, V, P, Z, work, uold, E, mytmp, tau)
       End Do
       write (*,*) "success"
 End Program OPEXPMULTTEST
