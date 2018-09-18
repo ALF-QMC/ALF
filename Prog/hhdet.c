efa682257c86db20ecd949607a9a888ce13b742a
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2018-09-18 18:53:35 +0200

    Add a C function for calculating our determinant. Various C functions for accessing tiles lack a Fortran equivalent

---
+++ Prog/hhdet.c
@@ -0,0 +1,10 @@
+#include <plasma.h>
+#include <plasma_descriptor.h>
+#include <plasma_types.h>
+
+/** Calculate the determinant of a householder object
+ */
+void hhdet(plasma_desc_t* T, plasma_complex64_t* phase)
+{
+    *phase = 1.0;
+}
