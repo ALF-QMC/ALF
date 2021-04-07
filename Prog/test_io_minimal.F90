1c3cc164cffaa82160c4e136f53e8c729ef65388
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2021-04-07 21:14:25 +0200

    test_io_minimal.F90: seed random number generator

--- Prog/test_io_minimal.F90
+++ Prog/test_io_minimal.F90
@@ -2,13 +2,33 @@ program test_io
   Use mpi
   Implicit none
 
-  Integer       :: size_conf, i, ierr, Irank
+  Integer       :: size_conf, i, ierr, Isize, Irank, SEED_IN, STATUS(MPI_STATUS_SIZE), k
   Real(Kind=Kind(0.d0)), allocatable :: conf(:)
   Real(Kind=Kind(0.d0)) :: x
   CHARACTER (LEN=64) :: file_conf
+  Integer, allocatable :: SEED_VEC(:)
   
   CALL MPI_INIT(ierr)
+  CALL MPI_COMM_SIZE(MPI_COMM_WORLD,ISIZE,IERR)
   CALL MPI_COMM_RANK(MPI_COMM_WORLD,IRANK,IERR)
+  
+  CALL RANDOM_SEED (SIZE=K)
+  ALLOCATE (SEED_VEC(K))
+  
+  IF (IRANK == 0) THEN
+    OPEN(UNIT=5, FILE='seeds', STATUS='OLD', ACTION='READ')
+    DO I = ISIZE-1,1,-1
+      READ (5,*) SEED_IN
+      CALL MPI_SEND(SEED_VEC, K, MPI_INTEGER, I, I+1024, MPI_COMM_WORLD,IERR)
+    ENDDO
+    READ(5,*) SEED_VEC
+    CLOSE(5)
+  ELSE
+    CALL MPI_RECV(SEED_IN, K, MPI_INTEGER,0,  IRANK + 1024,  MPI_COMM_WORLD,STATUS,IERR)
+  ENDIF
+  CALL RANSET(SEED_VEC)
+  CALL RANDOM_SEED (PUT=SEED_VEC)
+  DEALLOCATE (SEED_VEC)
   
   size_conf = 64000
   allocate(conf(size_conf))
@@ -34,3 +54,5 @@ program test_io
   CALL MPI_FINALIZE(ierr)
         
 end program
+
+
