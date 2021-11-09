d0e0a8c3439219dab8516e77b5d3a3caca3b3e2b
Florian Goth <fgoth@physik.uni-wuerzburg.de>
2021-11-09 21:34:51 +0100

    first tests

---
+++ testsuite/libmscbdecomp_mod/1-HomogeneousExp-lmult.F90
@@ -0,0 +1,70 @@
+! compile with
+! gfortran -I ../../Libraries/libmscbdecomp/ -L ../../Libraries/libmscbdecomp/ 1-HomogeneousExp-lmult.F90 -lmscbdecomp
+
+Program HomogeneousExplmult
+
+  Use HomogeneousSingleColExp_mod
+
+        COMPLEX (KIND=8) :: myx
+        
+        type(HomogeneousSingleColExp) :: test
+        integer, parameter :: nredges = 6
+        integer, parameter :: ndim = 24
+        Type(Node) :: nodes(nredges)
+        real(kind=kind(0.D0)) :: weight, sumdiag, sumoff
+        real(kind=kind(0.D0)), allocatable :: mys(:)
+        COMPLEX (KIND=8), DIMENSION(ndim, ndim) :: mat
+        integer :: i
+        
+        allocate(mys(ndim))
+        nodes(1)%x=1
+        nodes(1)%y=3
+        nodes(1)%axy=0.1
+
+        nodes(2)%x=5
+        nodes(2)%y=7
+        nodes(2)%axy=0.1
+        
+        nodes(3)%x=9
+        nodes(3)%y=11
+        nodes(3)%axy=0.1
+        
+        nodes(4)%x=13
+        nodes(4)%y=15
+        nodes(4)%axy=0.1
+
+        nodes(5)%x=17
+        nodes(5)%y=19
+        nodes(5)%axy=0.1
+
+        nodes(6)%x=21
+        nodes(6)%y=23
+        nodes(6)%axy=0.1
+        
+        weight = 1.0
+        mys = 0.3
+        mat = 0
+        do i = 1, ndim
+            mat(i,i) = 1
+        enddo
+
+        call test%init(nodes, nredges, mys, weight)
+        call test%lmult(mat)
+        call test%lmultinv(mat)
+        sumdiag = 0
+        sumoff = 0
+        do i = 1, ndim
+            sumdiag = sumdiag + DBLE(mat(i,i))
+        enddo
+        do i = 1, ndim-3
+            sumoff = sumoff + DBLE(mat(i,i+2))
+        enddo
+        write (*,*) sumoff, sumdiag
+        if (abs(sumdiag - ndim) > ndim*1E-15) then
+        ERROR STOP 2
+        endif
+        if (abs(sumoff) > 1E-15) then !FIXME: this limit is a bit scale less...
+        ERROR STOP 4
+        endif
+        write (*,*) "success"
+end Program HomogeneousExplmult
