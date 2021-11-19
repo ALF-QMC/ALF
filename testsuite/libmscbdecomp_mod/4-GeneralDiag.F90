6eb63348a82729cde660b8fe9b41fc97d9381e06
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-11-19 03:18:16 +0100

    some interesting test.

--- testsuite/libmscbdecomp_mod/4-GeneralDiag.F90
+++ testsuite/libmscbdecomp_mod/4-GeneralDiag.F90
@@ -43,7 +43,19 @@ Program GeneralExpTest
         
         weight = 1.0
         mys = 0.0 ! initialize chemical potential to zero
-        
+        mys(1) = 0.2
+        mys(3) = -0.4
+        mys(5) = 0.6
+        mys(7) = -0.4
+        mys(9) = 0.6
+        mys(11) = 0.7
+        mys(13) = 0.8
+        mys(15) = -0.11
+        mys(17) = 1
+        mys(19) = -1.3
+        mys(21) = 1.2
+        mys(23) = 1.57
+
         ! initialize as identity matrix
         mat = 0
         do i = 1, ndim
