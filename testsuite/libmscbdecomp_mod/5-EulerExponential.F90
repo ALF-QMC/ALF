6c4f62ff96edd5cad72a26b6e2eceeef8adb0375
Florian Goth <fgoth@physik.uni-wuerzburg.de>
2021-11-29 14:36:39 +0100

    improve test

--- testsuite/libmscbdecomp_mod/5-EulerExponential.F90
+++ testsuite/libmscbdecomp_mod/5-EulerExponential.F90
@@ -1,14 +1,14 @@
 ! compile with
 ! gfortran -I ../../Libraries/libmscbdecomp/ -L ../../Libraries/libmscbdecomp/ 1-ZeroDiag-lmult.F90 -lmscbdecomp
 
-subroutine exectest(nodes, nredges, ndim, mys)
+subroutine exectest(gd, ndim, mys)
   Use Exponentials_mod
+  Use colorvertex_mod
   implicit none
         type(EulerExp) :: ee
-        integer :: nredges
         integer :: ndim
         integer, parameter :: usedcolors = 1
-        Type(Node) :: nodes(nredges)
+        Type(GraphData) :: gd
         real(kind=kind(0.D0)) :: weight, sumdiag, sumoff
         real(kind=kind(0.D0)), allocatable :: mys(:)
         COMPLEX (KIND=kind(0.D0)), DIMENSION(ndim, ndim) :: mat
@@ -21,10 +21,14 @@ subroutine exectest(nodes, nredges, ndim, mys)
         do i = 1, ndim
             mat(i,i) = 1
         enddo
-
-        call ee%init(nodes, usedcolors, mys, weight)
+        
+        ee = createEulerExponentialfromGraphData(gd, mys)
         
         call ee%lmult(mat)
+        call ee%lmult(mat)
+        call ee%lmult(mat)
+        call ee%lmultinv(mat)
+        call ee%lmultinv(mat)
         call ee%lmultinv(mat)
 
         ! test for Trace(mat) = ndim
@@ -45,9 +49,49 @@ subroutine exectest(nodes, nredges, ndim, mys)
         endif
         
 
+        ! initialize as identity matrix
+        mat = 0
+        do i = 1, ndim
+            mat(i,i) = 1
+        enddo
+        
 
         call ee%rmult(mat)
+        call ee%rmult(mat)
+        call ee%rmult(mat)
         call ee%rmultinv(mat)
+        call ee%rmultinv(mat)
+        call ee%rmultinv(mat)
+        ! test for Trace(mat) = ndim
+        sumdiag = 0
+        sumoff = 0
+        do i = 1, ndim
+            sumdiag = sumdiag + DBLE(mat(i,i))
+        enddo
+        do i = 1, ndim-3
+            sumoff = sumoff + DBLE(mat(i,i+2))
+        enddo
+        write(*,*) "rmult  ", sumoff, sumdiag
+        if (abs(sumdiag - ndim) > ndim*1E-15) then
+        ERROR STOP 3
+        endif
+        if (abs(sumoff) > 1E-15) then !FIXME: this limit is a bit scale less...
+        ERROR STOP 6
+        endif
+
+        ! initialize as identity matrix
+        mat = 0
+        do i = 1, ndim
+            mat(i,i) = 1
+        enddo
+        
+        call ee%lmult_T(mat)
+        call ee%lmult_T(mat)
+        call ee%lmult_T(mat)
+        call ee%lmultinv_T(mat)
+        call ee%lmultinv_T(mat)
+        call ee%lmultinv_T(mat)
+
         ! test for Trace(mat) = ndim
         sumdiag = 0
         sumoff = 0
@@ -59,11 +103,49 @@ subroutine exectest(nodes, nredges, ndim, mys)
         enddo
         write (*,*) sumoff, sumdiag
         if (abs(sumdiag - ndim) > ndim*1E-15) then
-        ERROR STOP 3
+        ERROR STOP 102
         endif
         if (abs(sumoff) > 1E-15) then !FIXME: this limit is a bit scale less...
-        ERROR STOP 6
+        ERROR STOP 104
         endif
+        
+
+        ! initialize as identity matrix
+        mat = 0
+        do i = 1, ndim
+            mat(i,i) = 1
+        enddo
+        
+
+        call ee%rmult_T(mat)
+!         call ee%rmult_T(mat)
+!         call ee%rmult_T(mat)
+!         call ee%rmult_T(mat)
+!         call ee%rmult_T(mat)
+!         call ee%rmult_T(mat)
+!         call ee%rmultinv_T(mat)
+!         call ee%rmultinv_T(mat)
+!         call ee%rmultinv_T(mat)
+!         call ee%rmultinv_T(mat)
+!         call ee%rmultinv_T(mat)
+        call ee%rmultinv_T(mat)
+        ! test for Trace(mat) = ndim
+        sumdiag = 0
+        sumoff = 0
+        do i = 1, ndim
+            sumdiag = sumdiag + DBLE(mat(i,i))
+        enddo
+        do i = 1, ndim-3
+            sumoff = sumoff + DBLE(mat(i,i+2))
+        enddo
+        write (*,*) "rmult_T", sumoff, sumdiag
+        if (abs(sumdiag - ndim) > ndim*1E-15) then
+        ERROR STOP 103
+        endif
+        if (abs(sumoff) > 1E-15) then !FIXME: this limit is a bit scale less...
+        ERROR STOP 106
+        endif
+        
 
         call ee%adjoint_over_two(mat)
         ! test for Trace(mat) = ndim
@@ -90,104 +172,69 @@ end subroutine
 Program EulerExpTest
 
   Use Exponentials_mod
+  Use MvG_mod
   
   interface
-  subroutine exectest(nodes, nredges, ndim, mys)
+  subroutine exectest(gd, ndim, mys)
   Use Exponentials_mod
+  Use colorvertex_mod
         integer :: nredges
         integer :: ndim
-        Type(Node) :: nodes(nredges)
+        Type(GraphData) :: gd
         real(kind=kind(0.D0)), allocatable :: mys(:)
     end subroutine
   end interface
         type(EulerExp) :: ee
-        integer, parameter :: nredges = 6
-        integer, parameter :: ndim = 24
-        integer, parameter :: usedcolors = 1
-        Type(Node) :: nodes(nredges)
+!         integer, parameter :: nredges = 6
+        integer, parameter :: ndim = 36
+!         integer, parameter :: usedcolors = 1
+        type(GraphData) :: gd
         real(kind=kind(0.D0)) :: weight, sumdiag, sumoff
         real(kind=kind(0.D0)), allocatable :: mys(:)
-        COMPLEX (KIND=kind(0.D0)), DIMENSION(ndim, ndim) :: mat
-        integer :: i
+        COMPLEX (KIND=kind(0.D0)), DIMENSION(:,:), allocatable :: input
+        integer :: i, j
         
-        allocate(mys(ndim))
-        nodes(1)%x=1
-        nodes(1)%y=3
-        nodes(1)%col=1
-        nodes(1)%axy=0.1
-
-        nodes(2)%x=5
-        nodes(2)%y=7
-        nodes(2)%col=1
-        nodes(2)%axy=0.1
+        allocate(mys(ndim), input(ndim, ndim) )
+        input = 0
+        do i = 1, ndim-1
+             input(i, i + 1) = 1
+             input(i + 1, i) = 1
+        enddo
         
-        nodes(3)%x=9
-        nodes(3)%y=11
-        nodes(3)%col=1
-        nodes(3)%axy=0.1
+        do i = 1, ndim-2
+             input(i, i + 2) = 1
+             input(i + 2, i) = 1
+        enddo
+        input = input * 0.1
         
-        nodes(4)%x=13
-        nodes(4)%y=15
-        nodes(4)%col=1
-        nodes(4)%axy=0.1
-
-        nodes(5)%x=17
-        nodes(5)%y=19
-        nodes(5)%col=1
-        nodes(5)%axy=0.1
-
-        nodes(6)%x=21
-        nodes(6)%y=23
-        nodes(6)%col=1
-        nodes(6)%axy=0.1
+        gd = mat2verts(input) ! convert to graphdata structure
+        call MvG_decomp(gd%verts) ! perform the decomposition
+        
+        call determine_used_colors_of_graph(gd)
+        write (*,*) "Nr edges: ", gd%nredges
+        if (gd%usedcolors == gd%deltag) then
+            write(*,*) "Maximum Degree", gd%deltag, ". Found", gd%usedcolors," Families -> optimal decomposition"
+        else
+            write(*,*) "Maximum Degree", gd%deltag, ". Found", gd%usedcolors," Families"
+        endif
 
         ! Start with zero diagonals
         mys = 0.0 ! initialize chemical potential to zero
-        call exectest(nodes, nredges, ndim, mys)
+        call exectest(gd, ndim, mys)
         
         ! Now test homogeneous exponentials
-        mys(1) = 0.5
-        mys(3) = 0.5
-        mys(5) = 0.5
-        mys(7) = 0.5
-        mys(9) = 0.5
-        mys(11) = 0.5
-        mys(13) = 0.5
-        mys(15) = 0.5
-        mys(17) = 0.5
-        mys(19) = 0.5
-        mys(21) = 0.5
-        mys(23) = 0.5
-        call exectest(nodes, nredges, ndim, mys)
+        mys = 0.5
+        call exectest(gd, ndim, mys)
         
         ! Now test traceless exponentials
-        mys(1) = 0.5
-        mys(3) = -0.5
-        mys(5) = 0.5
-        mys(7) = -0.5
-        mys(9) = 0.5
-        mys(11) = -0.5
-        mys(13) = 0.5
-        mys(15) = -0.5
-        mys(17) = 0.5
-        mys(19) = -0.5
-        mys(21) = 0.5
-        mys(23) = -0.5
-        call exectest(nodes, nredges, ndim, mys)        
+        
+!         call exectest(gd, ndim, mys)        
         
         ! Now test general exponentials
-        mys(1) = 0.5
-        mys(3) = -0.6
-        mys(5) = 0.5
-        mys(7) = -0.7
-        mys(9) = 0.5
-        mys(11) = -0.9
-        mys(13) = 0.5
-        mys(15) = -0.5
-        mys(17) = 0.2
-        mys(19) = -0.5
-        mys(21) = 0.1
-        mys(23) = -0.5
-        call exectest(nodes, nredges, ndim, mys)        
+      do i = 1, ndim
+        mys(i) = 0.1*i
+      enddo
+       call exectest(gd, ndim, mys)
+        deallocate(input)
         write (*,*) "success"
 end Program EulerExpTest
