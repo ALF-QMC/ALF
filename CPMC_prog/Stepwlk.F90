7e939824391f9df9e2d67b9f988fe56bd3dd9138
zhliu <zihong.liu@physik.uni-wuerzburg.de>
2023-11-01 21:56:32 +0100

    Update

--- CPMC_prog/Stepwlk.F90
+++ CPMC_prog/Stepwlk.F90
@@ -349,19 +349,25 @@
           Do nt = ltrot_bp, 1, -1
             
              Do i_wlk = 1, N_wlk
-             Do nf_eff = 1,N_FL_eff
+
+                if ( weight_k(i_wlk) .gt. Zero ) then
                 
-                nf=Calc_Fl_map(nf_eff)
-                Call Hop_mod_mmthlc_1D2(phi_bp_l(nf_eff,i_wlk)%U,nf,1)
+                Do nf_eff = 1,N_FL_eff
+                   
+                   nf=Calc_Fl_map(nf_eff)
+                   Call Hop_mod_mmthlc_1D2(phi_bp_l(nf_eff,i_wlk)%U,nf,1)
 
-                Do n = N_op,1,-1
+                   Do n = N_op,1,-1
 
-                   Call Op_mmultR(phi_bp_l(nf_eff,i_wlk)%U,Op_V(n,nf),nsigma_bp(i_wlk)%f(n,nt),'c',1)
+                      Call Op_mmultR(phi_bp_l(nf_eff,i_wlk)%U,Op_V(n,nf),nsigma_bp(i_wlk)%f(n,nt),'c',1)
+                   enddo
+                
+                   Call Hop_mod_mmthlc_1D2(phi_bp_l(nf_eff,i_wlk)%U,nf,1)
+
                 enddo
-             
-                Call Hop_mod_mmthlc_1D2(phi_bp_l(nf_eff,i_wlk)%U,nf,1)
 
-             enddo
+                endif
+
              Enddo
              
              if ( ( mod(nt, Nwrap) .eq. 0 ) .or. ( nt .eq. 1 ) ) then
