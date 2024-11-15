41bc1731bebc2eeb0a463433a444491304917989
Fakher Assaad <assaad@physik.uni-wuerzburg.de>
2024-11-15 16:44:03 +0100

    LRC  works for all  lattices.

--- Prog/Hamiltonians/Hamiltonian_LRC_smod.F90
+++ Prog/Hamiltonians/Hamiltonian_LRC_smod.F90
@@ -405,7 +405,9 @@
           Case ("KAGOME")
              Call  Set_Default_hopping_parameters_kagome(Hopping_Matrix,Ham_T_vec, Ham_Chem_vec, Phi_X_vec, Phi_Y_vec, &
                   &                                      Bulk, N_Phi_vec, N_FL, List, Invlist, Latt, Latt_unit )
-
+          Case Default 
+             Write(error_unit,*) 'Your lattice is not supported for the t-V model. '
+             CALL Terminate_on_error(ERROR_HAMILTONIAN,__FILE__,__LINE__)
           end Select
 
           Call  Predefined_Hoppings_set_OPT(Hopping_Matrix,List,Invlist,Latt,  Latt_unit,  Dtau, Checkerboard, Symm, OP_T )
