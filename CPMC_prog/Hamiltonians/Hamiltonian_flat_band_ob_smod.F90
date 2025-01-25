1569578154ae1ccbd05cfc4312189ff104c26893
zhliu <steinsgatesp@gmail.com>
2025-01-25 21:27:29 +0100

    Fix bug

--- CPMC_prog/Hamiltonians/Hamiltonian_flat_band_ob_smod.F90
+++ CPMC_prog/Hamiltonians/Hamiltonian_flat_band_ob_smod.F90
@@ -877,6 +877,7 @@
              end do
           end do
           zpot = zn*ham_v3*zv3
+          obs_scal(2)%obs_vec(1) = obs_scal(2)%obs_vec(1) + zpot*z_fac
 
           zrho = cmplx(0.d0, 0.d0, kind(0.d0))
           do nf = 1, n_fl
